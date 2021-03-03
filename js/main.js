acad2b2e9704ee3b799b5f7a821d5b196d257ff4
nikiul <nikulans@gmail.com>
2021-03-03 16:16:07 +0300

    fix rating btn

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ const ratingCard = document.querySelectorAll('.rating__card');
 if(ratingCard) {
     for (let card of ratingCard) {
         const cardLearnMoreBtn = card.querySelector('.card__btn');
-        const learnMore = card.querySelector('.card__learn-more')
+        const learnMore = card.querySelector('.card__learn-more');
+        const learnMoreImg = card.querySelector('.card__btn-img');
+        const learnMoreText = card.querySelector('.card__btn-text');
 
         cardLearnMoreBtn.addEventListener('click', function(){
             learnMore.classList.toggle('hide');
+            learnMoreImg.classList.toggle('card__btn-img--active');
+            // learnMoreText.classList.toggle('card__btn-text--active');
+            learnMoreText.innerHTML = (learnMoreText.innerHTML == 'Show less') ? 'Learn more' : 'Show less' ;
         })
     }
 }
